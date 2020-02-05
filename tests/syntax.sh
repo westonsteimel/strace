69208e705187dd7c2d3ade4bd4834bdf47647b78
Eugene Syromyatnikov <evgsyr@gmail.com>
2020-02-05 20:37:27 +0100

    tests/syntax: add suffices to produced log/exp files
    In order to enable inspection of them.
    
    * tests/syntax.sh (log_sfx, log_path, exp_path): New functions.
    (check_exit_status_and_stderr, check_exit_status_and_stderr_using_grep,
    check_e, check_e_using_grep, check_h): Call log_path/exp_path instead
    of using LOG/EXP variables.

--- tests/syntax.sh
+++ tests/syntax.sh
@@ -10,28 +10,43 @@
 
 . "${srcdir=.}/init.sh"
 
+log_sfx()
+{
+	printf "%s" "$*" | tr -c '0-9A-Za-z-=,' '_' | head -c 128
+}
+
+log_path()
+{
+	printf "%s.%s" "$LOG" "$(log_sfx "$*")"
+}
+
+exp_path()
+{
+	printf "%s.%s" "$EXP" "$(log_sfx "$*")"
+}
+
 check_exit_status_and_stderr()
 {
-	$STRACE "$@" 2> "$LOG" &&
+	$STRACE "$@" 2> "$(log_path "$*")" &&
 		dump_log_and_fail_with \
 			"strace $* failed to handle the error properly"
-	match_diff "$LOG" "$EXP" \
+	match_diff "$(log_path "$*")" "$(exp_path "$*")" \
 		"strace $* failed to print expected diagnostics"
 }
 
 check_exit_status_and_stderr_using_grep()
 {
-	$STRACE "$@" 2> "$LOG" &&
+	$STRACE "$@" 2> "$(log_path "$*")" &&
 		dump_log_and_fail_with \
 			"strace $* failed to handle the error properly"
-	match_grep "$LOG" "$EXP" \
+	match_grep "$(log_path "$*")" "$(exp_path "$*")" \
 		"strace $* failed to print expected diagnostics"
 }
 
 check_e()
 {
 	local pattern="$1"; shift
-	cat > "$EXP" << __EOF__
+	cat > "$(exp_path "$*")" << __EOF__
 $STRACE_EXE: $pattern
 __EOF__
 	check_exit_status_and_stderr "$@"
@@ -40,7 +55,7 @@ __EOF__
 check_e_using_grep()
 {
 	local pattern="$1"; shift
-	cat > "$EXP" << __EOF__
+	cat > "$(exp_path "$*")" << __EOF__
 $STRACE_EXE: $pattern
 __EOF__
 	check_exit_status_and_stderr_using_grep "$@"
@@ -56,6 +71,6 @@ check_h()
 				printf '%s: %s\n' "$STRACE_EXE" "$pattern"
 			done
 		printf "Try '%s -h' for more information.\\n" "$STRACE_EXE"
-	} > "$EXP"
+	} > "$(exp_path "$*")"
 	check_exit_status_and_stderr "$@"
 }
